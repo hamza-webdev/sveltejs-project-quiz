a223db6451a536109f23c89536f496759a6979d3
hamza-webhelpmedica <hamza.bedoui@webhelpmedica.com>
2020-05-03 20:46:32 +0200

    install svero router

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,33 @@
 <script>
-	import Quiz from './Quiz.svelte';
-	export let name="hamza";
+
+	import { Router, Route } from 'svero';
+
+	import Header from './Header.svelte';
+	import Index from './pages/Index.svelte';
+	import About from './pages/About.svelte';
+
 </script>
+<style>
 
-<h1> hello Quiz with Svelte !! ;)  </h1>
-<Quiz quizName="New QUIZ" />
-<hr>
+	:global(body) {
+		/* this will apply to <body> */
+		margin: 0;
+		padding: 0;
+	}
 
-<Quiz  title />
+	.container {
+		width: 80%;
+		margin: 4em auto;
+	}
+
+</style>
+
+<Header/>
+
+<div class="container">
+	<Router>
+		<Route path="*" component={Index} />
+		<Route path="/about" component={About} />
+
+	</Router>
+</div>
---
+++ src/pages/Index.svelte
@@ -0,0 +1,11 @@
+<h1>I'm homeee</h1>
+
+<script>
+
+    let bro = 'Bro';
+
+</script>
+
+<h1>{bro}</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, laborum dignissimos? Ab blanditiis veniam a aspernatur autem, harum, quia dolor pariatur labore asperiores recusandae nihil dolorem exercitationem id itaque tempora?</p>
