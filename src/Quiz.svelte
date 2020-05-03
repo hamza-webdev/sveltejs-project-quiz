291dc1a96dd132649fe169542dd58a29735e2cc2
hamza-webhelpmedica <hamza.bedoui@webhelpmedica.com>
2020-05-03 19:28:16 +0200

    create component Quiz.svelte

---
+++ src/Quiz.svelte
@@ -0,0 +1,9 @@
+<script>
+   // export const quizName = "hamza Quiz"; // const: can't change it an export in component file App.svelte
+   export let quizName = "hamza Quiz";
+</script>
+
+<div>
+    <h2>Voici liste de quiz a répondre :</h2>
+    <h3>{quizName}</h3>
+</div>
